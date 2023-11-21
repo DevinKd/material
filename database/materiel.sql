3595779f86174757cb0caabb2b4be5e3f918366e
hunter <hao__chen@sina.com>
2023-11-21 18:24:34 +0800

    Update database

--- database/materiel.sql
+++ database/materiel.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `materiel` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `materiel`;
 -- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
 --
 -- Host: localhost    Database: materiel
@@ -385,4 +387,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-11-21 17:08:22
+-- Dump completed on 2023-11-21 18:23:41
